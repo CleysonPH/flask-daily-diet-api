c6585e629f97db11b046b908afaefc9888acea9e
Cleyson <cleysonph@gmail.com>
2024-01-18 18:01:07 -0300

    chore: add database config

--- dailydiet/__init__.py
+++ dailydiet/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    from dailydiet.ext import database
+
+    database.init_app(app)
+
     from dailydiet.routers import ping
 
     app.register_blueprint(ping.bp)
