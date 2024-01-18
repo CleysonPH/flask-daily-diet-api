a14038bcfd9f6f3780a7649544ac451776c57e5f
Cleyson <cleysonph@gmail.com>
2024-01-18 19:57:31 -0300

    test: add tests for update meal by id route

--- tests/e2e/test_meal.py
+++ tests/e2e/test_meal.py
@@ -54,3 +54,30 @@ def test_delete_meal_not_found(client):
     response = client.delete("/api/meals/1")
     assert response.status_code == 404
     assert response.json["message"] == "Meal not found"
+
+
+def test_update_meal(client, meals):
+    meal = meals[0]
+    request_body = {
+        "name": "Breakfast Edited",
+        "description": "A delicious breakfast Edited",
+        "datetime": "2019-01-01T08:00:00.000",
+        "in_diet": True,
+    }
+    response = client.put(f"/api/meals/{meal.id}", json=request_body)
+    assert response.status_code == 200
+    assert response.json["id"] == meal.id
+    assert response.json["name"] == request_body["name"]
+    assert response.json["description"] == request_body["description"]
+
+
+def test_update_meal_not_found(client):
+    request_body = {
+        "name": "Breakfast Edited",
+        "description": "A delicious breakfast Edited",
+        "datetime": "2019-01-01T08:00:00.000",
+        "in_diet": True,
+    }
+    response = client.put("/api/meals/1", json=request_body)
+    assert response.status_code == 404
+    assert response.json["message"] == "Meal not found"
