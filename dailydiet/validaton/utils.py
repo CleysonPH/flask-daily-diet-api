9950fa41b1668487d27e072ea060b059cb67231d
Cleyson <cleysonph@gmail.com>
2024-01-18 22:42:24 -0300

    chore: fix line length

--- dailydiet/validaton/utils.py
+++ dailydiet/validaton/utils.py
@@ -8,4 +8,6 @@ def convert_pydantic_validation_error_to_field_errors(e):
         if field not in errors:
             errors[field] = []
         errors[field].append(error["msg"])
-    return ValidationError([FieldError(field, errors[field]) for field in errors])
+    return ValidationError(
+        [FieldError(field, errors[field]) for field in errors]
+    )
